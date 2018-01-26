07ff044adf64e4fa649fd4bfbcfeaeb8971e647e
Rob Rose <RobRoseKnows@gmail.com>
2018-01-26 17:52:04 -0500

    Working on adding CDN support.

---
+++ get_cdn.js
@@ -0,0 +1,11 @@
+let cdn_list = require('./cdn_list.json');
+
+class GitHubCornerCdns {
+	getSvgUrl(bg_color, octocat_color) {
+		if(bg_color in cdn_list) {
+			
+		}
+	}
+}
+
+module.exports = GitHubCornerCdns;
