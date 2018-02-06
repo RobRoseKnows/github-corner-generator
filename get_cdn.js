3a7c4328f995a0eaa4df1f5a8441ef24c7328402
Rob Rose <RobRoseKnows@gmail.com>
2018-02-06 18:07:16 -0500

    Worked on CDN list and constructor
    - Worked on the constructor for the Corner creator class
    - Worked on the CDN code.
    - Added code for an Enum object.

--- get_cdn.js
+++ get_cdn.js
@@ -2,8 +2,10 @@ let cdn_list = require('./cdn_list.json');
 
 class GitHubCornerCdns {
 	getSvgUrl(bg_color, octocat_color) {
-		if(bg_color in cdn_list) {
-			
+		if(bg_color.toUpperCase() in cdn_list && octocat_color.toUpperCase() in cdn_list[bg_color]) {
+			return cdn_list[bg_color.toUpperCase()][octocat_color.toUpperCase()];
+		} else {
+			return "NONE";
 		}
 	}
 }
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 "use strict";
 
-class GitHubCorner {
-    constructor(repo_url = "example.com", bg_color = "#151513", octocat_color = "#fff", ) {
+var GitHubCornerCdns = require('./get_cdn.js');
+var Enum = require('./enum.js');
 
+const corner_side = new Enum('LEFT', 'RIGHT');
+
+class GitHubCorner {
+    constructor(repo_url = "example.com", bg_color = "#151513", octocat_color = "#fff", use_cdn = false) {
+    	this.repo_url = repo_url;
+    	this.bg_color = bg_color;
+    	this.octocat_color = octocat_color;
+    	this.use_cdn = use_cdn;
     }
 }
 
